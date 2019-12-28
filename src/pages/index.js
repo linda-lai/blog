3ad1eeb174a1106f7b6b43239dd73ca6453f999f
Linda Lai <lindalai.615@gmail.com>
2019-12-29 04:30:00 +1100

    Create blog post template and index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default function Index({ data }) {
+  const { edges: posts } = data.allMarkdownRemark
 
-export default IndexPage
+  return (
+    <Layout>
+      <div className="blog-posts">
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }) => {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+                <p>{post.excerpt}</p>
+              </div>
+            )
+          })}
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { Helmet } from 'react-helmet'
 // import '../css/blog-post.css'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query we'll write in a bit
 }) {
   const { markdownRemark: post } = data // data.markdownRemark holds your post data
   return (
-    <div className="blog-post-container">
-      <Helmet title={`${post.frontmatter.title}`}/>
-      <div
-        className="blog-post-content"
-        dangerouslySetInnerHTML={{ __html: post.html }}>
+
+    <Layout>
+      <SEO title="Page two" />
+      <div className="blog-post-container">
+        <Helmet title={`${post.frontmatter.title}`}/>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}>
+        </div>
       </div>
-    </div>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
   )
 }
 
