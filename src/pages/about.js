7ddc969ca951c57cb28509b3e9f3d86e025b10d3
Linda Lai <lindalai.615@gmail.com>
2020-01-01 19:13:27 +1100

    Update default font

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,12 @@ import Layout from '../components/layout'
 const About = () => {
   return (
     <Layout>
-      <div>About Me</div>
+      <h1>About Me</h1>
+      <p>
+      Hi, hello, hey! I'm Linda, a developing developer, code-blooded female and all round Hermione learning (and loving!) to code the software eating the world today. Perpetually curious, relentlessly enthusiastic and unabashedly dorky - that's me.
+      </p>
     </Layout>
   )
 }
-
-
 
 export default About
