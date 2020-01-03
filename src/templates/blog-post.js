d40739ce4f73bb6ee729db5de27c1a8f2a01a5d7
Linda Lai <lindalai.615@gmail.com>
2020-01-04 02:12:09 +1100

    feat(responsive design): Implement media queries for responsive navigation Update syntax for consistency Remove unused/commented out code

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { graphql, Link } from "gatsby"
-import Img from 'gatsby-image'
+// import Img from 'gatsby-image'
 import { Helmet } from 'react-helmet'
 import styles from '../styles/blog-post.module.css'
 
 import Layout from "./layout"
 import SEO from "../components/seo"
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query we'll write in a bit
-}) {
+const Template = ({
+  data, // this prop will be injected by the GraphQL query
+}) => {
   const { markdownRemark: post } = data // data.markdownRemark holds your post data
   // let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
   return (
 
     <Layout>
-      <SEO title="Page two" />
+      <SEO title={`${post.frontmatter.title}`} />
       <div className={styles.blogTemplateContainer}>
         <Helmet title={`${post.frontmatter.title}`}/>
         <div
@@ -29,6 +29,8 @@ export default function Template({
   </Layout>
   )
 }
+
+export default Template
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
