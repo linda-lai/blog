e3a9dd1a6bd18e8d18ddc5a6e367042c17ae7cf7
Linda Lai <lindalai.615@gmail.com>
2020-01-09 00:31:20 +1100

    Resize logo and spacing

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ const About = () => {
         <p>
           Hi, hello, hey! I'm Linda. Programming, puns and people - that's my jam. After feeling restless and dissatisfied with my career trajectory a few years ago, I began dipping my toes into digital and development eating the world today, found the water was warm, and haven't look back. I took the plunge and recently completed a Diploma of Information Technology at Coder Academy learning full-stack with Ruby/Rails, JavaScript and the MERN stack.
         </p>
-        <img src={logo} alt=""/>
+        <img src={logo} alt="Lindalogical logo" className={styles.aboutLogo} />
       </div>
     </Layout>
   )
