e65ed24396f48a4b5db5cef1d7f4aed7aaf1556b
Linda Lai <lindalai.615@gmail.com>
2020-01-11 17:13:29 +1100

    feat: Add Prop Types, code cleanup

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,9 +5,6 @@ const Intro = () => {
   return (
     <div className={styles.intro}>
       <h1>Hi, hello, hey! I'm Linda.</h1>
-      {/* <div>
-        <img src="https://source.unsplash.com/random/1600x500" alt=""/>
-      </div> */}
       <h2>
         Often, I write code. Occasionally, I write about tech, pop culture, politics or any other flim-flam that piques my interest.
       </h2>
