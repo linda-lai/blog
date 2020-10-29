ede64f51d31c0dfcc988be76030d31b17c99a4ad
Linda Lai <lindalai.615@gmail.com>
2020-10-29 17:47:07 +1100

    chore: Update footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,20 +4,40 @@ import styles from '../styles/footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      © {new Date().getFullYear()}{` `}
+      By
+      {` `}
       <a
         href='https://www.linda-lai.com'
         target='_blank'
         rel='noreferrer noopener'>
         Linda Lai
-      </a>,
-      Built with
+      </a>
+      ,
+      {` `}
+      say 👋 on
       {` `}
       <a
-        href='https://www.gatsbyjs.org'
+        href='https://github.com/linda-lai'
         target='_blank'
         rel='noreferrer noopener'>
-        Gatsby
+        GitHub
+      </a>
+      ,
+      {` `}
+      <a
+        href='https://twitter.com/lindalogical'
+        target='_blank'
+        rel='noreferrer noopener'>
+        Twitter
+      </a>
+      {` `}
+      or
+      {` `}
+      <a
+        href='https://www.linkedin.com/in/lindalai'
+        target='_blank'
+        rel='noreferrer noopener'>
+        LinkedIn
       </a>
     </footer>
   )
