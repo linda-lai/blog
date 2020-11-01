3583871422daadc8c3d3e47d84977241ca40479a
Linda Lai <lindalai.615@gmail.com>
2020-11-02 10:40:51 +1100

    accessibility: Replace divs, rearrange tags for better semantic HTML

--- src/components/hero-title.js
+++ src/components/hero-title.js
@@ -3,14 +3,10 @@ import styles from '../styles/hero-title.module.css'
 
 const HeroTitle = (props) => {
   return (
-    <div className={styles.heroTitle}>
-      <div className="heroTitleWrapper">
-        <h1>
-          {props.title}
-        </h1>
-        <h2>{props.description}</h2>
-      </div>
-    </div >
+    <header className={styles.heroTitle}>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </header >
   )
 }
 
